7a3f04d932e023a8d25ffe695e15931c46ea3fd8
Laura Brustenga i Moncusí <brust@math.ku.dk>
2023-03-29 19:09:57 +0200

    add docs for Model Kit functions

--- src/GridMethod.jl
+++ src/GridMethod.jl
@@ -29,11 +29,13 @@ function __init__()
     @require HomotopyContinuation = "f213a82b-91d6-5c5d-acf7-10f1c761b327" begin
         # import .HomotopyContinuation as HC
         import .HomotopyContinuation.ModelKit as MK
+        include("ModelingKits.jl")
         include("HC.ModelKit.jl")
     end
     @require ModelingToolkit = "961ee093-0014-501f-94e3-6117800e7a78" begin
         @eval import Symbolics
         import .ModelingToolkit as MTK
+        include("ModelingKits.jl")
         include("ModelingToolkit.jl")
     end
 end
