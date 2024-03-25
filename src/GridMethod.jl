a8c024083c0428f8c66be84bfdd2098c01dcb6b7
Chris La Valle <lavalle.l.c@gmail.com>
2024-03-24 19:24:05 -0500

    Cleans up modules

--- src/GridMethod.jl
+++ src/GridMethod.jl
@@ -1,22 +1,9 @@
 module GridMethod
 
-#Indicate modules for use
-using UnPack # @unpack, @pack!
-using DocStringExtensions: SIGNATURES, TYPEDEF
-
-using Combinatorics
-import LinearAlgebra as LA
-
-using DocStringExtensions
-
 #Include files with extracode
 include("Polynomial.jl")
 using .Polynomial
 
-#export rand_poly
-#include("Utils.jl")
-
-#export Onorm, Ocond, Wnorm, Wcond
 include("Norms.jl")
 using .Norms
 
@@ -31,23 +18,4 @@ using .Coordinates
 
 include("Han.jl")
 using .Han
-
-include("GridPlot.jl")
-using .GridPlot
-
-#using Requires
-#
-#include("ModelKitDocs.jl")
-#function __init__()
-#    @require HomotopyContinuation = "f213a82b-91d6-5c5d-acf7-10f1c761b327" begin
-#        import .HomotopyContinuation.ModelKit as MK
-#        include("HC.ModelKit.jl")
-#    end
-#    @require ModelingToolkit = "961ee093-0014-501f-94e3-6117800e7a78" begin
-#        import .ModelingToolkit as MTK
-#        include("ModelingToolkit.jl")
-#    end
-#end
-
-
 end
--- test/testplots.jl
+++ test/testplots.jl
@@ -3,7 +3,6 @@ const HCMK = ModelKit
 
 using Plots
 using GridMethod.GridModule
-using GridMethod.GridPlot
 using GridMethod.Polynomial
 using GridMethod.Han
 
